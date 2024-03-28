56044debe851cdc51002c05d406aab54db13748a
Raşit Çolakel <rasitcolakel@hotmail.com>
2024-03-29 00:45:15 +0300

    Add nativewind environment type definition and tailwind configuration

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,14 @@
-import { View, Text } from "react-native";
+import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { SafeAreaView, Text } from "react-native";
 
-type Props = {};
-
-const App = (props: Props) => {
+export default function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <SafeAreaView className="flex-1">
+      <StatusBar style="auto" />
+      <Text className="text-2xl">
+        Open up App.tsx to start working on your app!
+      </Text>
+    </SafeAreaView>
   );
-};
-
-export default App;
+}
