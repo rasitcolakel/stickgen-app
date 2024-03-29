264e0d180b2b54eca16ed49c94feace7f0a0152f
Raşit Çolakel <rasitcolakel@hotmail.com>
2024-03-29 23:38:12 +0300

    Add KeyboardAvoidingView component and cn utility function

---
+++ app/components/KeyboardAvoidingView.tsx
@@ -0,0 +1,43 @@
+import {
+  Platform,
+  KeyboardAvoidingView as RNKeyboardAvoidingView,
+  ScrollView,
+} from "react-native";
+
+import { cn } from "app/lib/utils";
+
+export interface KeyboardAvoidingViewProps
+  extends React.ComponentPropsWithoutRef<typeof RNKeyboardAvoidingView> {
+  scrollEnabled?: boolean;
+  scrollViewProps?: React.ComponentPropsWithoutRef<typeof ScrollView>;
+}
+
+function KeyboardAvoidingView({
+  className,
+  scrollEnabled = false,
+  ...props
+}: KeyboardAvoidingViewProps) {
+  if (scrollEnabled) {
+    return (
+      <RNKeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        className="flex-1"
+        {...props}
+      >
+        <ScrollView {...props.scrollViewProps} className={cn(className)}>
+          {props.children}
+        </ScrollView>
+      </RNKeyboardAvoidingView>
+    );
+  }
+
+  return (
+    <RNKeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      className={cn("flex-1", className)}
+      {...props}
+    />
+  );
+}
+
+export { KeyboardAvoidingView };
